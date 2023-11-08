f6a52b6c71e164f863048f5b2deec78953a04c21
Akmalov-Asror <akmalovasror0@gmail.com>
2023-11-08 13:42:23 +0300

    news

---
+++ TestProject/Domains/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace TestProject.Domains;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Title { get; set; }    
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
