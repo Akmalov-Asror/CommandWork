004899cb2ad5957a6b7fa804405d0de74d85771f
Akmalov-Asror <akmalovasror0@gmail.com>
2023-11-12 20:13:27 +0300

    news

--- TestProject/Controllers/HomeController.cs
+++ TestProject/Controllers/HomeController.cs
@@ -12,4 +12,5 @@ public class HomeController : Controller
     public IActionResult Index() => View();
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    
 }
